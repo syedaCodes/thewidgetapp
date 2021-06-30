2a1c58afd3030a103f14c71e7596441ee8c72e3c
syedaCodes <syedaa.codes@gmail.com>
2021-06-30 19:39:10 +0530

    search widget complete

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -2,36 +2,58 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-    const [ term, getTerm ] = useState('');
-    const [ results, setResults ] = useState([]) 
+    const [ term, getTerm ] = useState('programming');
+    const [ debouncedTerm, setDebouncedTerm ] = useState(term);
+    const [ results, setResults ] = useState([]);
 
     useEffect(() => {
 
-        const findTerm = async () =>{
+        const timerId = setTimeout(() => {
+                setDebouncedTerm(term)
+        }, 1000);
+
+        return() => {
+            clearTimeout(timerId);
+        }
+    },[term]);
+
+    useEffect(() => {
+        //fetch req and update term and setResults
+        const findTerm = async () => {
+
             const { data } = await axios.get('https://en.wikipedia.org/w/api.php',{
                 params:{
                     action: 'query',
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    srsearch: term
+                    srsearch: debouncedTerm
                 }
             });
 
             setResults(data.query.search);
         };
 
-        if(term){
+        if(debouncedTerm){
             findTerm();
         }
 
-    }, [term]);
+    }, [debouncedTerm]);
+
 
     const renderResults = results.map((result) => {
+        const regex = /(<([^>]+)>)/gi; 
+        const cleanSnippet = result.snippet.replace(regex, "");
+
             return (
-                <div className="result" key={result.pageid}>
+                <div className="result"
+                key={result.pageid}
+                >
                     <h2 title="" className="result__title">{result.title}</h2>
-                    <p className="result__desc">{result.snippet}</p>
+                    <div className="result__content">
+                        <p className="result__desc">{cleanSnippet}</p>
+                        <a href={`https://en.wikipedia.org?curid=${result.pageid}`}>Go</a>
+                    </div>
                 </div>
             )
         });
